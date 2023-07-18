f2f83ed06fb15e042c52f6244edb999ff7c40906
g-code99 <net.git.ketza@gmail.com>
2023-07-18 12:04:47 -0600

    Actualizacion de algoritmo para crear un arbol lex
    * En el archivo dentro de utils se puede crear un automata
    * No se pueden crear Nodos intermedios con el mismo nombre
    * La secuencia del árbol debe de ser la misma que la del documento
    * Se ha modificado la funcion createLexTree y printLex

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/g-code99/arboles/analizador-lexico-automatas/utils"
 )
@@ -42,8 +43,7 @@ func inorderTraversal(node *Node) {
 			inorderTraversal(child) // Recorre los otros hijos del nodo actual
 		}
 	} else {
-		if input[cnt] != []byte(node.Value)[0] {
-			fmt.Println("NDO")
+		if cnt < len(input) && input[cnt] != []byte(node.Value)[0] {
 			valid = false
 		}
 		fmt.Printf("%s ", node.Value)
@@ -51,10 +51,69 @@ func inorderTraversal(node *Node) {
 	}
 }
 
+func create(data string) *Node {
+	lines := strings.Split(data, "\n")
+	for v := range lines {
+		fmt.Println(v)
+	}
+
+	return &Node{}
+}
+
+func createLexTree(data string) *Node {
+	lines := strings.Split(data, "\n")
+	nodesMap := make(map[string]*Node)
+
+	// Primero, creamos todos los nodos sin hijos
+	for _, line := range lines {
+		parts := strings.Split(line, "|")
+		value := parts[0]
+		nodesMap[value] = &Node{Value: value}
+	}
+
+	// Luego, agregamos los hijos a los nodos
+	for _, line := range lines {
+		parts := strings.Split(line, "|")
+		value := parts[0]
+		childrenData := parts[1]
+
+		if len(childrenData) > 0 {
+			children := strings.Split(childrenData, ",")
+			for _, childValue := range children {
+				childNode, exists := nodesMap[childValue]
+				if exists {
+					nodesMap[value].Child = append(nodesMap[value].Child, childNode)
+				} else {
+					// Crear un nuevo nodo hoja y agregarlo como hijo
+					newChildNode := &Node{Value: childValue}
+					nodesMap[value].Child = append(nodesMap[value].Child, newChildNode)
+				}
+			}
+		}
+	}
+
+	return nodesMap["S"]
+}
+
+func printLex(node *Node, depth int) {
+	if node == nil {
+		return
+	}
+
+	fmt.Printf("%sValue: %s\n", strings.Repeat("  ", depth), node.Value)
+	for _, child := range node.Child {
+		printLex(child, depth+1)
+	}
+}
+
 func main() {
-	// Leer archivo
-	line := utils.ReadFile("ejer1.txt")
-	fmt.Println(line)
+	data := strings.Trim(utils.ReadFile("ejer1.txt"), "\n")
+	// data := "S|M,N,Z|\nM|x,Z,b|\nZ|b,a|\nN|b|\nZ|X,N|\nX|a|\nN|a,b,x|"
+	// fmt.Println(data)
+	lex := createLexTree(data)
+	// fmt.Println(utils.ReadFile("ejer1.txt"))
+	fmt.Println("Arbol léxico creado:")
+	printLex(lex, 0)
 
 	// pedir datos
 	scanner := bufio.NewScanner(os.Stdin)
@@ -62,82 +121,84 @@ func main() {
 	scanner.Scan()
 	input = scanner.Bytes()
 
-	// Raiz del arbol
-	root := &Node{
-		Value: "S",
-		Child: []*Node{
-			{
-				Value: "M",
-				Child: []*Node{
-					{
-						Value: "x",
-						Child: nil,
-					},
-					{
-						Value: "Z",
-						Child: []*Node{
-							{
-								Value: "b",
-								Child: nil,
-							},
-							{
-								Value: "a",
-								Child: nil,
-							},
-						},
-					},
-					{
-						Value: "b",
-						Child: nil,
-					},
-				},
-			},
-			{
-				Value: "N",
-				Child: []*Node{
-					{
-						Value: "b",
-						Child: nil,
-					},
-				},
-			},
-			{
-				Value: "Z",
-				Child: []*Node{
-					{
-						Value: "X",
-						Child: []*Node{
-							{
-								Value: "a",
-								Child: nil,
-							},
-						},
-					},
-					{
-						Value: "N",
-						Child: []*Node{
-							{
-								Value: "a",
-								Child: nil,
-							},
-							{
-								Value: "b",
-								Child: nil,
-							},
-							{
-								Value: "x",
-								Child: nil,
-							},
-						},
-					},
-				},
-			},
-		},
-	}
+	// // Raiz del arbol
+	// root := &Node{
+	// 	Value: "S",
+	// 	Child: []*Node{
+	// 		{
+	// 			Value: "M",
+	// 			Child: []*Node{
+	// 				{
+	// 					Value: "x",
+	// 					Child: nil,
+	// 				},
+	// 				{
+	// 					Value: "Z",
+	// 					Child: []*Node{
+	// 						{
+	// 							Value: "b",
+	// 							Child: nil,
+	// 						},
+	// 						{
+	// 							Value: "a",
+	// 							Child: nil,
+	// 						},
+	// 					},
+	// 				},
+	// 				{
+	// 					Value: "b",
+	// 					Child: nil,
+	// 				},
+	// 			},
+	// 		},
+	// 		{
+	// 			Value: "N",
+	// 			Child: []*Node{
+	// 				{
+	// 					Value: "b",
+	// 					Child: nil,
+	// 				},
+	// 			},
+	// 		},
+	// 		{
+	// 			Value: "Z",
+	// 			Child: []*Node{
+	// 				{
+	// 					Value: "X",
+	// 					Child: []*Node{
+	// 						{
+	// 							Value: "a",
+	// 							Child: nil,
+	// 						},
+	// 					},
+	// 				},
+	// 				{
+	// 					Value: "N",
+	// 					Child: []*Node{
+	// 						{
+	// 							Value: "a",
+	// 							Child: nil,
+	// 						},
+	// 						{
+	// 							Value: "b",
+	// 							Child: nil,
+	// 						},
+	// 						{
+	// 							Value: "x",
+	// 							Child: nil,
+	// 						},
+	// 					},
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// }
+
+	// // fmt.Println(root)
 
 	// Realizar la búsqueda infija en el árbol
 	fmt.Print("Búsqueda: ")
-	inorderTraversal(root)
+	inorderTraversal(lex)
 	fmt.Println()
 	fmt.Printf("Expresión válida: %v\n", valid)
 }
